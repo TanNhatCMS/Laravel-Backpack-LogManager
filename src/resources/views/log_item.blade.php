1cf2783ade12087f773c8b7b2b6fdafb8e86d480
makss <makss@users.noreply.github.com>
2022-12-29 11:59:32 +0200

    Scroll and Open #collapse

--- src/resources/views/log_item.blade.php
+++ src/resources/views/log_item.blade.php
@@ -6,6 +6,7 @@
     trans('backpack::logmanager.log_manager') => route('log.index'),
     trans('backpack::logmanager.preview') => false,
   ];
+  $sizeLog = count($logs);
 @endphp
 
 @section('header')
@@ -21,14 +22,14 @@
   <div id="accordion" role="tablist" aria-multiselectable="true">
     @forelse($logs as $key => $log)
       <div class="card mb-0 pb-0">
-        <div class="card-header bg-{{ $log['level_class'] }}" role="tab" id="heading{{ $key }}">
-            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $key }}" aria-expanded="true" aria-controls="collapse{{ $key }}" class="text-white">
+        <div class="card-header bg-{{ $log['level_class'] }}" role="tab" id="heading{{ $sizeLog - $key }}">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $sizeLog - $key }}" aria-expanded="true" aria-controls="collapse{{ $sizeLog - $key }}" class="text-white">
               <i class="la la-{{ $log['level_img'] }}"></i>
               <span>[{{ $log['date'] }}]</span>
               {{ Str::limit($log['text'], 150) }}
             </a>
         </div>
-        <div id="collapse{{ $key }}" class="panel-collapse collapse p-3" role="tabpanel" aria-labelledby="heading{{ $key }}">
+        <div id="collapse{{ $sizeLog - $key }}" class="panel-collapse collapse p-3" role="tabpanel" aria-labelledby="heading{{ $sizeLog - $key }}">
           <div class="panel-body">
             <p>{{$log['text']}}</p>
             <pre><code class="php">{{ trim($log['stack']) }}</code></pre>
@@ -46,4 +47,13 @@
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/default.min.css">
   <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
   <script>hljs.initHighlightingOnLoad();</script>
+  <script type="text/javascript">
+    jQuery(document).ready(function($) {
+        // Scroll and Open, if #collapse exists
+        if (location.hash.match(/^#collapse/) && $(location.hash).length > 0) {
+            $(location.hash).collapse('show');
+            $('html, body').animate({scrollTop: $(location.hash).closest('.card').offset().top}, 500);
+        }
+    });
+  </script>
 @endsection
